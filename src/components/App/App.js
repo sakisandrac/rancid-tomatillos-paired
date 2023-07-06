cd0f767bb5ac8ccf69591c328c93c8ae34e033e7
Calli Herrmann <calliherrmann@gmail.com>
2023-07-06 18:49:10 -0400

    Add app functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Homepage from '../Homepage/Homepage';
+import movieData from '../../mock-data/mock-movies';
+import { useState } from 'react';
 
 const App = () => {
+  const [movies, setMovies] = useState(movieData.movies)
+
   return (
     <div className="App">
+      <h1>Rancid Tomatillos</h1>
+      <Homepage movies={movies}/>
     </div>
   );
 }
