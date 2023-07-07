5a836a673bd19a0fb526fbdb9e2c4f14e4c57a12
Calli Herrmann <calliherrmann@gmail.com>
2023-07-07 15:43:17 -0400

    Add error handling on homepage for network request

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,5 +1,9 @@
 const getMovies = async () => {
     let response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    if(!response.ok) {
+        console.log(response.statusText)
+        throw new Error(response.statusText)
+    }
     let data = response.json()
     return data
 }
