bb011837415bbd64f0fbe9c4aaab3fdaa7234a60
“Saki <sakisandra.c@gmail.com>
2023-07-10 14:38:15 -0600

    Add intercept to stub data for tests

--- cypress/e2e/moviedetails_spec.cy.js
+++ cypress/e2e/moviedetails_spec.cy.js
@@ -2,15 +2,27 @@ describe('Movie Details Page', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.get('figure').first().click()
+      .intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', {
+        statusCode: 200,
+        fixture: 'moviedetails'
+      })
+    .get('figure').first().click()
   })
 
   it('Should be able to see movie details page', () => {
+    //add check for URL
     cy.get('h1').should('have.text', 'Rancid Tomatillos')
     .get('.movie-backdrop-container').find('img')
     .get('.movie-title').contains('p', 'Black Adam')
+    .get('.movie-details').contains('p', 'Action')
     .get('.movie-details-container').contains('p', 'Release Date: 2022-10-19')
     .get('p').last().should('have.text', 'Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.')
+  })
+
+  it('Should be able to click back button to go back to homepage', () => {
+    cy.get('button').click()
+    .get('.movies-container').find('.poster')
+    //add check for URL
   })
 
   it('Should load the movie detials page with nav bar details', () => {
@@ -18,11 +30,4 @@ describe('Movie Details Page', () => {
     .get('.nav-bar').find('img')
   })
 
-  //user clicks on the movie page, and sees the movie title, movie image, and movie deatils box
-  
-  //movie details box contains rating, runtime, release date and summary
-  
-  //user can click the back button and be brought back to the homepage 
-
-  //user will see nav bar
 })
