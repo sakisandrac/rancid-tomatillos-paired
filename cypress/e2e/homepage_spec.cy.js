1c522c8afc4b3f58226b1ec6ff2dfe948930bce3
Calli Herrmann <calliherrmann@gmail.com>
2023-07-10 16:38:24 -0400

    Add interception for get request for all movies being tested

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,10 +1,17 @@
 describe('Homepage navigation', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: 'homepage'
+    })
   });
   it('Should load the homepage with all movies', () => {
+    //add check for url - .url().should('include', '/')
     cy.get('main').contains('h1', 'Rancid Tomatillos')
+    .get('figure').first().find('img')
     .get('.movie-title-homepage').first().should('have.text', 'Black Adam')
+    .get('figure').last().find('img')
     .get('.movie-title-homepage').last().should('have.text', 'X')
   })
   it('Should load the homepage with nav bar details', () => {
