9d16b666f397fbfbb493e5969aac01d2560e539c
Calli Herrmann <calliherrmann@gmail.com>
2023-07-13 18:50:09 -0400

    Add conditional to prevent users from searching nothing

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,11 +5,25 @@ import { Link } from 'react-router-dom'
 
 const Search = ({search, handleSearch}) => {
 
+  const handleForm = (e) => {
+    e.preventDefault()
+    if (!search) return
+  }
+
+  const renderIcon = () => <button className='search-btn' type='submit'><img src={searchSymbol} alt='search icon'/></button>
+
+  const renderLink = () => {
+    return <Link to={`/search/${search}`}><button className='search-btn' type='submit'><img src={searchSymbol} alt='search icon'/></button></Link>
+  }
+
   return (
-    <div className='search-container'>
-      <input onChange={(e) => { handleSearch(e) }} value={search} name="search" placeholder="Search Titles" />
-      <Link to={`/search/${search}`}><button className='search-btn'><img src={searchSymbol} alt='search icon'/></button></Link>
-    </div>
+    <form className='search-container' onSubmit={handleForm}>
+      <input onChange={(e) => { handleSearch(e) }} 
+        value={search} 
+        name="search" 
+        placeholder="Search Titles" />
+      {search ? renderLink() : renderIcon() }
+    </form>
   )
 }
 
