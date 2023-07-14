807cf29802c96cfaf2d16117d67cc5aa5d128965
Calli Herrmann <calliherrmann@gmail.com>
2023-07-13 20:01:53 -0400

    Add test for searching movie

--- cypress/e2e/searchresults_spec.cy.js
+++ cypress/e2e/searchresults_spec.cy.js
@@ -9,5 +9,13 @@ describe('Search navigation', () => {
   it('Should use the searchbar to search for a movie', () => {
     cy.get('input[name="search"]')
     .should('be.visible')
+    .type('adam{enter}')
+    .should('have.value','adam')
+    .url().should('include', 'search')
+    .url().should('include', 'adam')
+    .get('h2').should('have.text', 'Results for "adam"')
+    .get('figure').find('img')
+    .get('.movie-title-homepage').should('have.text', 'Black Adam')
   })
+
 })
